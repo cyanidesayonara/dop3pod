92eebec9cbfbd559c83e9f08b1b45e438480492f
Santtu Nykänen <santtu.nykanen@solidabis.com>
2020-10-10 19:43:29 +0300

    Add heroku setup

--- dopepod/settings.py
+++ dopepod/settings.py
@@ -16,7 +16,6 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -27,9 +26,6 @@ SECRET_KEY = '+ine78vtjs8#*4=be0y=awe*$nj5!v4drszczfl)@g*0o$=%_b'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
 # Application definition
 
@@ -118,8 +114,8 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS =
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+django_heroku.settings(locals())
